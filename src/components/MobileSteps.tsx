22d4c6f77a519fbfb98352b94701bcb7e42780b8
AslamtoIbrahim <aslamtoibrahim1@gmail.com>
2025-07-08 16:46:47 -0700

    feat: enable step switching for mobile screens

--- src/components/MobileSteps.tsx
+++ src/components/MobileSteps.tsx
@@ -1,14 +1,25 @@
-import StepButton from './StepButton'
+import { useSelector } from "react-redux";
+import StepButton from "./StepButton";
+import type { RootState } from "../store/storeSteps";
+import { useDispatch } from "react-redux";
+import { setStep } from "../store/sliceSteps";
 
 const MobileSteps = () => {
+  const maxStep = useSelector((state: RootState) => state.maxStep);
+  const dispatch = useDispatch();
+  const stepOnclickValue = (value: number) => {
+    if (maxStep >= value) {
+      dispatch(setStep(value));
+    }
+  };
   return (
-    <div className='w-full flex items-center justify-center gap-4 md:hidden my-4'>
-        <StepButton value={1} />
-        <StepButton value={2} />
-        <StepButton value={3} />
-        <StepButton value={4} />
-      </div>
-  ) 
-}
+    <div className="w-full flex items-center justify-center gap-4 md:hidden my-4">
+      <StepButton value={1} stepOnclick={stepOnclickValue} />
+      <StepButton value={2} stepOnclick={stepOnclickValue} />
+      <StepButton value={3} stepOnclick={stepOnclickValue} />
+      <StepButton value={4} stepOnclick={stepOnclickValue} />
+    </div>
+  );
+};
 
-export default MobileSteps
+export default MobileSteps;
--- src/components/StepButton.tsx
+++ src/components/StepButton.tsx
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../store/storeSteps";
 
-type props = { value: number };
+type props = { value: number; stepOnclick?: (value: number) => void };
 
-const StepButton = ({ value }: props) => {
+const StepButton = ({ value, stepOnclick }: props) => {
   const step = useSelector((state: RootState) => state.step);
   const colored = step === value;
   return (
     <button
+      onClick={() => stepOnclick && stepOnclick(value)}
       className={`w-10 h-10  border border-blue-200 rounded-full md:shrink-0
     ${colored ? "bg-cyan-200 text-black" : "text-white"}`}
     >
