34fd0038f696c86de5ab132d049092e3191c1b7c
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-07-14 13:24:17 +0100

    change

--- src/components/DesktopSteps.tsx
+++ src/components/DesktopSteps.tsx
@@ -7,8 +7,7 @@ const DesktopSteps = () => {
   const onClickStepButton = (value: number) => {
     dispatch(setStep(value));
   };
-  return (
-    <div
+   return (<div
       className="hidden md:block bg-desktop bg-no-repeat bg-cover bg-center rounded-xl bg-lime-300
      w-1/3 py-8 px-7 space-y-8"
     >
